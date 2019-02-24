98bb4a0f0277d61bb0144a5fb708de6525b1beaf
Bob_chemist <bob_chemist@mail.ru>
2019-02-24 18:31:49 +0300

    dynamic deletion of components

--- src/App.js
+++ src/App.js
@@ -20,8 +20,18 @@ class App extends Component {
     })
   }
 
-  changeTitleHandler = pageTitle => {
-    this.setState({pageTitle})
+  onChangeName = (name, index) => {
+    const car = this.state.cars[index];
+    car.name = name;
+    const cars = [...this.state.cars];
+    cars[index] = car;
+    this.setState({cars}); 
+  }
+
+  deleteHandler(index) {
+    const cars = [...this.state.cars];
+    cars.splice(index, 1);
+    this.setState({cars});
   }
 
   render() {
@@ -36,7 +46,8 @@ class App extends Component {
             key={index}
             name={car.name}
             year={car.year}
-            onChangeTitle={() => this.changeTitleHandler(car.name) }
+            onDelete={this.deleteHandler.bind(this, index)}
+            onChangeName={event => this.onChangeName(event.target.value, index) }
           />
         )
       }) 
