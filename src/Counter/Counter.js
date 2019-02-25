3cb9f374ec00a7b1aa92599f73334ee821477525
Bob_chemist <bob_chemist@mail.ru>
2019-02-26 01:12:56 +0300

    component return without <div> wrapper. Used methods with React.Fragment, Auxiliary wrapper and new simple wrapper <></>

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Auxiliary from './../hoc/Auxiliary'
 
 export default class Counter extends Component {
   state = {
@@ -12,11 +13,11 @@ export default class Counter extends Component {
   }
   
   render() {
-      return [        
-          <h2 key={'1'}>Counter {this.state.counter}</h2>,
-          <button key={'2'} onClick={this.addCounter}>+</button>,
-          <button key={'3'} onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>,        
-      ]
+      // return [        
+      //     <h2 key={'1'}>Counter {this.state.counter}</h2>,
+      //     <button key={'2'} onClick={this.addCounter}>+</button>,
+      //     <button key={'3'} onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>,        
+      // ];
       // return (
       //   <div>
       //     <h2>Counter {this.state.counter}</h2>
@@ -24,5 +25,26 @@ export default class Counter extends Component {
       //     <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
       //   </div>
       // )
+      // return (
+      //   <React.Fragment>
+      //     <h2>Counter {this.state.counter}</h2>
+      //     <button onClick={this.addCounter}>+</button>
+      //     <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
+      //   </React.Fragment>
+      // )
+      // return (
+      //   <Auxiliary>
+      //     <h2>Counter {this.state.counter}</h2>
+      //     <button onClick={this.addCounter}>+</button>
+      //     <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
+      //   </Auxiliary>
+      // )
+      return (
+        <>
+          <h2>Counter {this.state.counter}</h2>
+          <button onClick={this.addCounter}>+</button>
+          <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
+        </>
+      )
   }
 }
