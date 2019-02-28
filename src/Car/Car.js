4776111365e2576fadecb722775183b2edc60ef2
Bob_chemist <bob_chemist@mail.ru>
2019-02-28 11:16:54 +0300

    Refs: focus managing

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -5,6 +5,16 @@ import PropTypes from 'prop-types'
 
 class Car extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.inputRef = React.createRef();
+	}
+
+	componentDidMount() {
+		if (this.props.index === 1) {
+			this.inputRef.current.focus();
+		}
+	}
   render() {
         
 		const inputClasses = [classes.input];
@@ -26,6 +36,7 @@ class Car extends React.Component {
 				<h3>Car name: {this.props.name}</h3>
 				<p>Year: {this.props.year}</p>
 				<input 
+					ref={this.inputRef}
 					type='text' 
 					onChange={this.props.onChangeName} 
 					value={this.props.name}
@@ -40,6 +51,7 @@ class Car extends React.Component {
 Car.propTypes = {
 	name: PropTypes.string.isRequired,
 	year: PropTypes.number,
+	index: PropTypes.number,
 	onChangeName: PropTypes.func,
 	onDelete: PropTypes.func,
 }
