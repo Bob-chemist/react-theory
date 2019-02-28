ea5cc0c771270b95bc9f1b83f4598e0fa282b189
Bob_chemist <bob_chemist@mail.ru>
2019-02-28 10:25:40 +0300

    simple high order component is added to wrap component with div with a specified class name

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,52 +1,11 @@
 import React from 'react'
-//import Radium from 'radium'
 import classes from './Car.css'
+import withClass from '../hoc/withClass'
 
 class Car extends React.Component {
 
-  componentWillReceiveProps(nextProps) {
-    console.log('Car componentWillReceiveProps', nextProps);
-    
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('Car shouldComponentUpdate', nextProps, nextState);
-    return nextProps.name.trim() !== this.props.name.trim();
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log('Car componentWillUpdate', nextProps, nextState);
-    
-  }
-
-  // static getDerivedStateFromProps(nextProps, prevState) {
-  //   console.log('Car getDerivedStateFromProps', nextProps, prevState);
-  //   return prevState;
-  // }
-
-  componentDidUpdate() {
-    console.log('Car componentDidUpdate');
-    
-  }
-
-  // getSnapshotBeforeUpdate() {
-  //   console.log('Car getSnapshotBeforeUpdate');
-    
-  // }
-
-  componentWillUnmount() {
-    console.log('Car componentWillUnmount');
-    
-  }
-
-	render() {
-    console.log('Car render');
-
-    // if (Math.random() > 0.7) {
-    //   throw Error('Car failed');
-      
-    // }
-    
+  render() {
+        
 		const inputClasses = [classes.input];
 
 		if (this.props.name !== '') {
@@ -59,18 +18,10 @@ class Car extends React.Component {
 			inputClasses.push(classes.bold);
 		}
 
-		const style = {
-			border: '1px solid gray',
-			boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-			':hover': {
-				border: '1px solid #aaa',
-				boxShadow: '0 4px 15px rgba(0,0,0,.25)',
-				cursor: 'pointer',
-			}
-		}
+	
 		
 		return ( 
-			<div className={classes.Car} style={style}>
+			<React.Fragment>
 				<h3>Car name: {this.props.name}</h3>
 				<p>Year: {this.props.year}</p>
 				<input 
@@ -80,9 +31,9 @@ class Car extends React.Component {
 					className={inputClasses.join(' ')}
 				/>
 				<button onClick={this.props.onDelete}>Delete!</button>
-			</div>  
+			</React.Fragment>  
 		)
 	}
 }
 
-export default Car
+export default withClass(Car, classes.Car);
